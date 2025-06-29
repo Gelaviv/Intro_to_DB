b1be8c5dee39efc17a6a88a5fb02a97d9bf9f517
Gelaviv <odunsiamarachi@gmail.com>
2025-06-29 19:38:55 +0100

    Removed the space in foreign key query

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -37,8 +37,8 @@ CREATE TABLE Orders(
 CREATE TABLE Order_Details(
     orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT,
-    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     book_id INT,
-    FOREIGN KEY(book_id) REFERENCES Books(book_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE
 );
