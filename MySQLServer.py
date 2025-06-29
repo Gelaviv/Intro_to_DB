2e30a37ac3e1a910470c8b5decfbaece0f9a414d
Gelaviv <odunsiamarachi@gmail.com>
2025-06-29 21:11:04 +0100

    Creating database with python script

---
+++ MySQLServer.py
@@ -0,0 +1,54 @@
+import mysql.connector
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def my_db_connector():
+    try:
+        connected = mysql.connector.connect(
+        host = os.getenv("DB_HOST"),
+        user = os.getenv("DB_USER"),
+        password = os.getenv("DB_PASSWORD"),
+        database = os.getenv("DB_NAME")
+        )
+        print ("Mysql is connected")
+        return connected
+
+    except Exception as e:
+        print(f"Error in conenecting to DB: {e}")
+        return None
+    
+
+def create_database():
+    try:
+        # Connect without specifying a database to create a new one
+        connection = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD")
+        )
+        
+        if connection.is_connected():
+            cursor = connection.cursor()
+            
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            connection.commit()
+            
+            print("Database 'alx_book_store' created successfully!")
+            
+    except Exception as e:
+        print(f"Error creating database: {e}")
+    finally:
+        if 'connection' in locals() and connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+if __name__ == "__main__":
+    
+    create_database()
+
+my_db_connector()
+
+
